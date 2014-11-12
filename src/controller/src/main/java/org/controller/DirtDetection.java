ed58c8b7167649fbce8c4002018496d9942cf162
Adam Podraza <apodra86@gmail.com>
2014-11-12 15:31:39 -0600

    added sub-poms

--- src/controller/src/main/java/org/controller/DirtDetection.java
+++
@@ -1,6 +0,0 @@
-package controller.src.main.java.org.controller;
-
-public class DirtDetection {
-	public Boolean isClean;
-	
-}
--- src/controller/src/main/java/org/controller/MainStart.java
+++
@@ -1,8 +0,0 @@
-package controller.src.main.java.org.controller;
-
-public class MainStart {
-	public static void main(String[] args) throws InterruptedException {
-			Controller.getInstance().initEverything();
-	}
-}
-
